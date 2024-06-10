ef471b2874dc60ec01f712c2498e5bb10cb6cd93
Beshar-Elhatib <besharelhatib@gmail.com>
2024-06-10 08:54:04 +0300

    Add project files.

---
+++ Makine/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Makine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.LineAnnotation lineAnnotation1 = new System.Windows.Forms.DataVisualization.Charting.LineAnnotation();
+            System.Windows.Forms.DataVisualization.Charting.LineAnnotation lineAnnotation2 = new System.Windows.Forms.DataVisualization.Charting.LineAnnotation();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(265, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(402, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(116, 133);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 26;
+            this.dataGridView1.Size = new System.Drawing.Size(641, 364);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            lineAnnotation1.Name = "LineAnnotation1";
+            lineAnnotation2.Name = "LineAnnotation2";
+            this.chart1.Annotations.Add(lineAnnotation1);
+            this.chart1.Annotations.Add(lineAnnotation2);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(832, 157);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Hedef Miktar";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Üretilen Miktar";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(565, 364);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "Makinelerin Grafiki ";
+            this.chart1.Titles.Add(title1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(902, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(378, 69);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Çiz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Makine Seçimi :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Başlangıç Tarihi :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(173, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Bitiş Tarihi :";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(265, 58);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2024, 6, 28, 0, 0, 0, 0);
+            this.dateTimePicker1.MinDate = new System.DateTime(2024, 6, 25, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(402, 24);
+            this.dateTimePicker1.TabIndex = 7;
+            this.dateTimePicker1.Value = new System.DateTime(2024, 6, 25, 0, 0, 0, 0);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(265, 88);
+            this.dateTimePicker2.MaxDate = new System.DateTime(2024, 6, 28, 0, 0, 0, 0);
+            this.dateTimePicker2.MinDate = new System.DateTime(2024, 6, 25, 0, 0, 0, 0);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(402, 24);
+            this.dateTimePicker2.TabIndex = 8;
+            this.dateTimePicker2.Value = new System.DateTime(2024, 6, 25, 0, 0, 0, 0);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1585, 621);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+    }
+}
+
---
+++ Makine/Form1.cs
@@ -0,0 +1,104 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Makine
+{
+    public partial class Form1 : Form
+    {
+        DataTable dataTable = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+            PopulateMachineDropdown();
+        }
+
+        private void LoadData()
+        {
+            dataTable.Columns.Add("Makine", typeof(string));
+            dataTable.Columns.Add("Tarih", typeof(DateTime));
+            dataTable.Columns.Add("HedefMiktar", typeof(int));
+            dataTable.Columns.Add("UretimMiktar", typeof(int));
+
+            string[] lines = File.ReadAllLines(@"E:\C#\ödevler\SonÖdevler\Ödev1\MakineBilgiler.txt");
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+
+                if (parts.Length == 4)
+                {
+                    string Makine = parts[0];
+                    DateTime Tarih = DateTime.ParseExact(parts[1], "dd/MM/yyyy", null);
+                    int HedefMiktar = int.Parse(parts[2]);
+                    int UretimMiktar = int.Parse(parts[3]);
+                    dataTable.Rows.Add(Makine, Tarih, HedefMiktar, UretimMiktar);
+                }
+            }
+            dataGridView1.DataSource = dataTable;
+        }
+        //-----------------------------------------
+        private void PopulateMachineDropdown()
+        {                                                                                    //Distinct() tekrar edilen değirler siler 
+            var machines = dataTable.AsEnumerable().Select(row => row.Field<string>("Makine")).Distinct().ToList();
+            machines.Insert(0, "All");                                                       //ToList()    sonuç dizie  gönderir       
+            comboBox1.DataSource = machines;
+        }
+        //-----------------------------------------
+        private void UpdateGridAndChart()
+        {
+            string selectedMachine = comboBox1.SelectedItem.ToString();
+            DateTime startDate = dateTimePicker1.Value;
+            DateTime endDate = dateTimePicker2.Value;
+
+            var filteredRows = dataTable.AsEnumerable()
+                .Where(row => (selectedMachine == "All" || row.Field<string>("Makine") == selectedMachine) &&
+                              row.Field<DateTime>("Tarih") >= startDate &&
+                              row.Field<DateTime>("Tarih") <= endDate);
+
+            var filteredTable = filteredRows.CopyToDataTable();
+            dataGridView1.DataSource = filteredTable;
+
+            chart1.Series.Clear();
+            var hedefMiktarSeries = new Series("Hedef Miktar")
+            {
+                ChartType = SeriesChartType.Column,
+                XValueType = ChartValueType.DateTime
+            };
+            var uretilenMiktarSeries = new Series("Uretilen Miktar")
+            {
+                ChartType = SeriesChartType.Column,
+                XValueType = ChartValueType.DateTime
+            };
+
+            foreach (DataRow row in filteredTable.Rows)
+            {
+                hedefMiktarSeries.Points.AddXY(row.Field<DateTime>("Tarih"), row.Field<int>("HedefMiktar"));
+                uretilenMiktarSeries.Points.AddXY(row.Field<DateTime>("Tarih"), row.Field<int>("UretimMiktar"));
+            }
+
+            chart1.Series.Add(hedefMiktarSeries);
+            chart1.Series.Add(uretilenMiktarSeries);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            UpdateGridAndChart();
+        }
+
+
+
+    }
+
+}
